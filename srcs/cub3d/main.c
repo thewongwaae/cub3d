922d588cdf19d439b5f3869bfb3957849641d038
Hong You Wong <hwong@u90z06s01.42kl.edu.my>
2023-03-30 12:37:37 +0800

    pending updates

---
+++ srcs/cub3d/main.c
@@ -0,0 +1,11 @@
+#include "cub3d.h"
+
+int	main( int ac, char **av )
+{
+	if (ac != 2)
+		return (write(1, "Error: Invalid arguments\n", 25));
+	// parse map
+	// hook inputs
+	// mlx loop
+	return (0);
+}
--- srcs/parse/keypress.c
+++ srcs/parse/keypress.c
@@ -3,19 +3,14 @@
 int	move( int keycode, t_game *game )
 {
 	if (keycode == 13 || keycode == 126)
-		// handle for up / forward
 		forward();
 	else if (keycode == 0 || keycode == 123)
-		// handle left
 		left();
 	else if (keycode == 1 || keycode == 125)
-		// handle down / backwards
 		backward();
 	else if (keycode == 2 || keycode == 124)
-		// handle right
 		right();
 	else if (keycode == 53)
-		// handle end
 		endgame();
 	return (0);
 }
