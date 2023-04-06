9ffb4077186590430686873040a135b631dcdeb5
Hong You Wong <hwong@u91z02s01.42kl.edu.my>
2023-04-06 14:46:42 +0800

    working on floodfill

--- srcs/cub3d/flood.c
+++ srcs/cub3d/flood.c
@@ -1,6 +1,20 @@
 #include "cub3d.h"
 
-int	check_map( t_game *game )
+static void	flood( int y, int x, int *l, t_game *game )
 {
 
 }
+
+int	check_map( t_game *game )
+{
+	int	leak;
+
+	leak = 0;
+	while (game->map[y][x] != '0')
+	{
+
+	}
+	flood()
+}
+
+// flood map, if out of bounds, map is not fully enclosed
