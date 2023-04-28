1499d293d32d42a630cd2fdf8bccbd5a45b7c204
Wong Hong You <wonghongyou@gmail.com>
2023-04-28 20:40:30 +0800

    uncommented fisheye fix and tweaked code to remove 0.67 magic value

--- srcs/render/raycast-3d.c
+++ srcs/render/raycast-3d.c
@@ -1,27 +1,29 @@
 #include "cub3d.h"
 
-// static float	fix_fisheye(t_game *g, float angle, int dist)
-// {
-// 	float	fisheye;
-// 	float	new_dist;
+static float	fix_fisheye(t_game *g, float angle, int dist)
+{
+	float	fisheye;
+	float	new_dist;
 
-// 	fisheye = g->p.pa - angle;
-// 	if (fisheye < 0)
-// 		fisheye += 2 * M_PI;
-// 	if (fisheye > 2 * M_PI)
-// 		fisheye -= 2 * M_PI;
-// 	new_dist = dist * cos(fisheye);
-// 	return (new_dist);
-// }
+	fisheye = g->p.pa - angle;
+	if (fisheye < 0)
+		fisheye += 2 * M_PI;
+	if (fisheye > 2 * M_PI)
+		fisheye -= 2 * M_PI;
+	new_dist = dist * cos(fisheye);
+	return (new_dist);
+}
 
 //need to fix, crash when trying to fill screen.
 static void	draw_3d(t_game *g, int constant, t_vec start, t_vec end)
 {
 	int ray_w;
 	int	ray_h;
+	int	ray_w_end;
 
 	ray_w = start.x;
-	while (ray_w < start.x + constant)
+	ray_w_end = (int)(start.x + constant + 0.5f);
+	while (ray_w < ray_w_end)
 	{
 		ray_h = start.y;
 		while(ray_h <= end.y)
@@ -55,19 +57,19 @@ void	cast_3d(t_game *g, int dist, int r, float angle)
 	t_vec	end;
 
 	(void) angle;
-	// dist = roundf(fix_fisheye(g, angle, dist));
+	dist = fix_fisheye(g, angle, dist);
 	/*
 		missing one ray because dividing normally doesnt return whole value,
 		and will round down.
 	*/
-	constant = (g->winsize.x / g->fovdeg) + 0.67; //WTF WHY .67
-	lineH = (g->winsize.y * CELL_SIZE) / dist;
+	constant = ((float)g->winsize.x / (float)g->fovdeg);
+	lineH = ((float)g->winsize.y * (float)CELL_SIZE) / (float)dist;
 	if (lineH > g->winsize.y)
 		lineH = g->winsize.y;
-	lineOff = (g->winsize.y / 2) - lineH / 2;
-	start = (t_vec){.x = (r * constant), .y = lineOff};
+	lineOff = ((float)g->winsize.y / 2.0f) - lineH / 2;
+	start = (t_vec){.x = (int)(r * constant), .y = (int)lineOff};
 	check_bounds(g->winsize, &start);
-	end = (t_vec){.x = (r * constant), .y = lineOff + lineH};
+	end = (t_vec){.x = (int)(r * constant), .y = (int)(lineOff + lineH)};
 	check_bounds(g->winsize, &end);
-	draw_3d(g, roundf(constant), start, end);
+	draw_3d(g, (int)(constant + 0.5f), start, end);
 }
